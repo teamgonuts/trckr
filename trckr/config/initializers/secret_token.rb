1ea9553552b756407fcd6274c8c809dda1e55820
Calvin Hawkes <calvinhawkes@gmail.com>
2013-01-11 15:11:46 -0800

    created rails project

---
+++ trckr/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trckr::Application.config.secret_token = 'fc1cd5249b3d433f9b102f9e3de4618d108988f8622f29116c78e87796171f130cd37a02edd2c1b7ae77a1853776c3abe6cacbbde2f6613a8948521f8d636916'
