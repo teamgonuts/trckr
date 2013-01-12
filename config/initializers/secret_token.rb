f924392f9a7f5b22a858e799bcb7c9e148d81042
Calvin Hawkes <calvinhawkes@gmail.com>
2013-01-12 09:53:19 -0800

    created new rails project

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Trckr::Application.config.secret_token = 'fc1cd5249b3d433f9b102f9e3de4618d108988f8622f29116c78e87796171f130cd37a02edd2c1b7ae77a1853776c3abe6cacbbde2f6613a8948521f8d636916'
+Trckr::Application.config.secret_token = '0cba02e62138c61dd2e42f39beffc3fc25aa0fe710d81e8537ac09d65799b0082a25673da31a98eb129b4a4855ddac45c7b8bee32572c42e030bfd4bfba3be91'
