b8e09ca0bb87b723a7bd0e20ba0bf85116c663a4
Calvin Hawkes <calvinhawkes@gmail.com>
2013-01-13 21:20:23 -0800

    added default route to tracker/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Trckr::Application.routes.draw do
-  get "tracker/index"
+  root :to => "tracker#index"
+  #get "tracker/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -56,5 +57,5 @@ Trckr::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  match ':controller(/:action(/:id))(.:format)'
 end
