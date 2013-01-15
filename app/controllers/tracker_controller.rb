e9cd6e4f6efd24ee8c7a31be12c5779e7bf0131d
Calvin Hawkes <calvinhawkes@gmail.com>
2013-01-15 14:36:37 -0800

    track params in url now

--- app/controllers/tracker_controller.rb
+++ app/controllers/tracker_controller.rb
@@ -6,4 +6,11 @@ class TrackerController < ApplicationController
     puts "navigating to track view"
     @id = params[:id]
   end
+
+  #redirects to track after retrieving the url parameters
+  #want a url parameter so users can link to the page
+  def track_helper
+    @id = params[:id]
+    redirect_to "/tracker/track/#{@id}"
+  end
 end
