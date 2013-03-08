fa719aa471e5d4dc162d3872850f7fde8dfb6109
Calvin Hawkes <calvinhawkes@gmail.com>
2013-03-08 09:52:45 -0800

    added detailed page for items (show action)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,17 @@
 class ItemsController < ApplicationController
 
+  #index defaults to list action
+  def index
+    list #calls list action
+    render('list') #renders list action
+  end
+
+
   def list
     @items = Item.order("items.id ASC")
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 end
