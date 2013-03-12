e489f1aefc63b3331f00edabc755793cca1ee5e6
Calvin Hawkes <calvinhawkes@gmail.com>
2013-03-12 12:03:06 -0700

    added sexy validation to adminUser model, regular validation to item model

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,21 @@ class AdminUser < ActiveRecord::Base
   
   has_many :item_edits
   has_many :items, :through => :item_edits
+
+  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+
+  #sexy validations
+  validates :first_name, :presence => true, :length => {:maximum => 40}
+
+  validates :last_name, :presence => true, :length => {:maximum => 40}
+
+  validates :username, :presence => true,
+                       :length => {:within => 3..25},
+                       :uniqueness => true
+
+  validates :email, :presence => true, :length => {:maximum => 100},
+                    :format => EMAIL_REGEX, :confirmation => true,
+                    :uniqueness => true
+                
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,13 @@ class Item < ActiveRecord::Base
   has_many :item_edits
   has_many :editors, :through => :item_edits, :class_name => "AdminUser"
 
-  validates_presence_of :name, :description, :origin, :original_owner
+  validates_presence_of :name, :description, :origin, :original_owner, :uid
+  validates_length_of :name, :origin, :original_owner, :maximum => 50
+  validates_length_of :description, :message, :maximum=> 255
+  validates_length_of :uid, :maximum => 40
+
   validates_uniqueness_of :uid
+
 
 
 end
